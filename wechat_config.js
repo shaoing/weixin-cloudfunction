230af1c778a2183bda8c424cf8cd102fcd4291bb
shaoing <shaoing@users.noreply.github.com>
2019-06-11 18:29:34 +0800

    Delete wechat_config.js

--- wechat_config.js
+++
@@ -1,9 +0,0 @@
-var wechat = {
-  appid:'wx888888888888',
-  mch_id:'1499999999999',
-  ip:'127.0.0.1',
-  notify_url:'https://api.mch.weixin.qq.com/pay/unifiedorder',
-  key:'8888888888888888888888888888888888',
-  url:'https://api.mch.weixin.qq.com/pay/unifiedorder'
-}
-module.exports = wechat;
