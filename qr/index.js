769cda9a394ad8f2c6e8fe6e25bfadb09adf30f9
shaoing <shaoing@users.noreply.github.com>
2019-06-11 18:26:38 +0800

    云函数获取小程序码

---
+++ qr/index.js
@@ -0,0 +1,14 @@
+const cloud = require('wx-server-sdk')
+cloud.init()
+exports.main = async (event, context) => {
+  try {
+    const result = await cloud.openapi.wxacode.getUnlimited({
+      scene: 'a=1'
+    })
+    console.log(result)
+    return result
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
